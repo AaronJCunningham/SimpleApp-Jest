70158176bb156e022aaed4972c65c673ab26ea93
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2020-11-01 12:06:08 +0100

    1st

---
+++ src/components/CommentBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class CommentBox extends React.Component {
+    render(){
+        return (
+            <from>
+                <h4>Add A Comment</h4>
+                <textarea />
+                <div>
+                    <button>Submit Content</button>
+                </div>
+            </from>
+        )
+    }
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CommentList = () => {
+    return <div>Comment Box</div>
+}
+
+export default CommentList;
+
